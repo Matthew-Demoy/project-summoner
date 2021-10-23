7990d3ba23712c1a7f653869b205edfb4c8cb897
Matthew Demoy <MatthewDemoy@student.uml.edu>
2021-10-23 09:23:20 -0400

    add balances, transfer summoners, deploy contract

---
+++ utils/addresses.ts
@@ -0,0 +1,5 @@
+export const addresses = {
+    rarity: "0xce761D788DF608BD21bdd59d6f4B54b2e27F25Bb",
+    rarityAtributes: "0xce761D788DF608BD21bdd59d6f4B54b2e27F25Bb",
+    random: "0x7426dBE5207C2b5DaC57d8e55F0959fcD99661D4"
+}
